d881a1daf8a26e71e65a0d1228b9102aad95c1d7
zeussecond16 <zeussecond16@gmail.com>
2026-04-06 15:07:17 +0700

    Update tugas_scraping.py

--- tugas_scraping.py
+++ tugas_scraping.py
@@ -7,20 +7,17 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Ambil judul
 judul = soup.find("h1").text.strip()
 
 items = soup.find_all("li")
 
 materi = [item.text.strip() for item in items]
 
-# Struktur data
 data = {
     "judul": judul,
     "materi_pembelajaran": materi
 }
 
-# Simpan ke JSON
 with open("C:/Users/bimaa/Downloads/hasil_scraping.json", "w", encoding="utf-8") as f:
     json.dump(data, f, indent=4, ensure_ascii=False)
 
